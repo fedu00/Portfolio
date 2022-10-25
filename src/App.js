98dae09eed315c7abd39afe9ef7ce5a31d7c1391
Wojtek F <fedakwojciech@gmail.com>
2022-10-25 12:30:17 +0200

    add animation to main sections and some styles to picture abut me

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 import Navbar from "./components/Navbar";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Contact from "./routes/Contact";
 import Projects from "./routes/Projects";
+
 import "./styles/appStyles.css";
 
 function App() {
+  const location = useLocation();
   return (
     <div className="websiteContainer">
       <Navbar />
-      <div className="contentContainer">
-        <Routes>
+      <AnimatePresence exitBeforeEnter>
+        <Routes location={location} key={location.pathname}>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </div>
+      </AnimatePresence>
     </div>
   );
 }
