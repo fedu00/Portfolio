0e12ffa88f4b564d7024ad7c8e4794a9202159a1
Wojtek F <fedakwojciech@gmail.com>
2022-11-22 17:02:41 +0100

    correct Project and DetailsProject

--- src/routes/Projects/Projects.js
+++ src/routes/Projects/Projects.js
@@ -1,97 +1,84 @@
 import Project from "../../components/project/Project";
 import "./projects.scss";
 import notes from "../../assetes/pictures/notes.png";
-// import geeknaut from "../assetes/pictures/geeknaut.png";
+import geeknaut from "../../assetes/pictures/geeknaut.png";
 import logo from "../../assetes/pictures/logo.png";
 import { useState } from "react";
 import ProjectDetails from "../../components/projectDetails/ProjectDetails";
 import AnimateSection from "../../components/AnimateSection";
 
 const Projects = () => {
-  const [adres, setAdres] = useState("");
-  const [secondText, setSecondText] = useState("");
+  const [adress, setAdress] = useState("");
+  const [text, setText] = useState("");
   const [title, setTitle] = useState("");
   const [image, setImage] = useState("");
   const [show, setShow] = useState(false);
 
-  const handleOpenProjects = (adres, image, title, secondText) => {
+  const handleOpenProjects = (adress, image, title, text) => {
     setTitle(title);
-    setAdres(adres);
-    setSecondText(secondText);
+    setAdress(adress);
+    setText(text);
     setImage(image);
     setShow(true);
   };
 
-  const obiekt = {
-    // geeknaut: {
-    //   title: "Geeknaut",
-    //   text: "przez rok pracy współtworzyłem takie projekty jak",
-    //   secondText: "dwadwa",
-    //   image: geeknaut,
-    // },
-    notes: {
-      title: "Notatnik",
-      adres: "https://github.com/fedu00/notes",
-      secondText:
-        "Prosta aplikacja napisana w React, wykorzystuje komponenty funkcyjne oraz zapisuje dane w locall store.",
-      image: notes,
-    },
-    logo: {
+  const projectsData = [
+    {
       title: "Protfolio",
-      adres: "https://github.com/fedu00/portfolio",
-      secondText:
-        "Strona samodziellnie zaprojektowana, wykorzystuje tutaj: React-router oraz Frame Motion. Portfolio jest jeszcze w trakcie tworzenia. ",
+      adress: "https://github.com/fedu00/portfolio",
       image: logo,
+      text: [
+        "Strona samodziellnie zaprojektowana, wykorzystuje tutaj: React-router oraz Frame Motion. Portfolio jest jeszcze w trakcie tworzenia. ",
+      ],
     },
-  };
+    {
+      title: "Notatnik",
+      adress: "https://github.com/fedu00/notes",
+      image: notes,
+      text: [
+        "Prosta aplikacja napisana w React, wykorzystuje komponenty funkcyjne oraz zapisuje dane w locall store.",
+      ],
+    },
+    {
+      title: "Geeknauts",
+      image: geeknaut,
+      text: [
+        "pracując dla firmy Geeknauts tworzyłem takie projekty jaki:",
+        "- Vip CLub",
+        "- Geeknauts Website",
+        "- Photovoltaica",
+        "- RCP",
+        "- Matura Testy i Zadania",
+      ],
+    },
+  ];
+
   return (
     <AnimateSection>
       <div className="projects-page-section">
         <div className="projects-container">
-          {/* <Project
-            image={geeknaut}
-            title={obiekt.geeknaut.title}
-            handleOpenProjects={() =>
-              handleOpenProjects(
-                obiekt.geeknaut.text,
-                obiekt.geeknaut.image,
-                obiekt.geeknaut.title,
-                obiekt.geeknaut.secondText
-              )
-            }
-          /> */}
-          <Project
-            image={notes}
-            title={obiekt.notes.title}
-            handleOpenProjects={() =>
-              handleOpenProjects(
-                obiekt.notes.adres,
-                obiekt.notes.image,
-                obiekt.notes.title,
-                obiekt.notes.secondText
-              )
-            }
-          />
-          <Project
-            image={logo}
-            title={obiekt.logo.title}
-            handleOpenProjects={() =>
-              handleOpenProjects(
-                obiekt.logo.adres,
-                obiekt.logo.image,
-                obiekt.logo.title,
-                obiekt.logo.secondText
-              )
-            }
-          />
+          {projectsData.map((element) => (
+            <Project
+              image={element.image}
+              title={element.title}
+              handleOpenProjects={() =>
+                handleOpenProjects(
+                  element.adress,
+                  element.image,
+                  element.title,
+                  element.text
+                )
+              }
+            />
+          ))}
         </div>
         {show && (
           <ProjectDetails
-            adres={adres}
+            adress={adress}
             setShow={setShow}
             image={image}
             title={title}
-            secondText={secondText}
+            text={text}
           />
         )}
       </div>
