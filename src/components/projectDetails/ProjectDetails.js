9ebed4319e9247ed360c16430d7b499a10756ef1
Wojtek F <fedakwojciech@gmail.com>
2023-03-01 16:28:16 +0100

    add handles cms and axios

--- src/components/projectDetails/ProjectDetails.js
+++ src/components/projectDetails/ProjectDetails.js
@@ -1,5 +1,10 @@
-import cross from '../../assetes/svgIcons/cross.svg';
-import { WrapperOffset, Wrapper, ImmageWrapper, TextWrapper } from 'components/projectDetails/ProjectDetails.styles'
+import cross from "../../assetes/svgIcons/cross.svg"
+import {
+  WrapperOffset,
+  Wrapper,
+  ImmageWrapper,
+  TextWrapper,
+} from "components/projectDetails/ProjectDetails.styles"
 
 const ProjectDetails = ({ adress, setShow, image, title, text }) => {
   return (
@@ -10,24 +15,20 @@ const ProjectDetails = ({ adress, setShow, image, title, text }) => {
           alt="logo"
           className="cross-icon"
           onClick={() => {
-            setShow(false);
+            setShow(false)
           }}
         />
-        <ImmageWrapper
-          image={image}
-        ></ImmageWrapper>
+        <ImmageWrapper image={image}></ImmageWrapper>
         <TextWrapper>
           <h1>{title}</h1>
           <a target="_blank" rel="noreferrer" href={adress}>
             {adress}
           </a>
-          {text.map((element) => (
-            <p>{element}</p>
-          ))}
+          <p>{text}</p>
         </TextWrapper>
       </Wrapper>
     </WrapperOffset>
-  );
-};
+  )
+}
 
-export default ProjectDetails;
+export default ProjectDetails
--- src/routes/Projects/Projects.js
+++ src/routes/Projects/Projects.js
@@ -1,9 +1,5 @@
 import Project from "../../components/project/Project"
-// import "./projects.scss";
-import portfolio from "../../assetes/pictures/portfolio.png"
-import geeknauts from "../../assetes/pictures/geeknauts.png"
-import studyBuddy from "../../assetes/pictures/studyBuddy.png"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import ProjectDetails from "../../components/projectDetails/ProjectDetails"
 import AnimateSection from "../../components/AnimateSection/AnimateSection"
 import SvgElement from "components/SvgElement/SvgElement"
@@ -21,6 +17,7 @@ import { ReactComponent as JsSVG } from "assetes/svgIcons/stackIcons/JS.svg"
 import { ReactComponent as TsSVG } from "assetes/svgIcons/stackIcons/TS.svg"
 import { ReactComponent as ReactSVG } from "assetes/svgIcons/stackIcons/react.svg"
 import { ReactComponent as FigmaSVG } from "assetes/svgIcons/stackIcons/figma.svg"
+import axios from "axios"
 
 const Projects = () => {
   const [adress, setAdress] = useState("")
@@ -28,6 +25,7 @@ const Projects = () => {
   const [title, setTitle] = useState("")
   const [image, setImage] = useState("")
   const [show, setShow] = useState(false)
+  const [projects, setProjects] = useState([])
 
   const handleOpenProjects = (adress, image, title, text) => {
     setTitle(title)
@@ -37,41 +35,38 @@ const Projects = () => {
     setShow(true)
   }
 
-  const projectsData = [
-    {
-      title: "Portfolio",
-      adress: "https://github.com/fedu00/portfolio",
-      image: portfolio,
-      text: [
-        "Samodzielnie zaprojektowana i napisana strona, do stworzenia jej wykorzystałem:",
-        "React",
-        "React-router",
-        "Frame Motion",
-        "Sass",
-        "Styled-components",
-      ],
-    },
-    {
-      title: "Geeknauts",
-      image: geeknauts,
-      text: [
-        "Podczas współpracy z firmą Geeknauts byłem odpowiedzialny za tworzenie części frontendowej głównie z wykorzystaniem biblioteki React i React Nastive, oraz tworzenie grafik i makiet za pomocą programu Figma w takich projektach jak:",
-        "- Vip CLub",
-        "- Geeknauts Website",
-        "- Photovoltaica",
-        "- RCP",
-        "- Matura Testy i Zadania",
-      ],
-    },
-    {
-      title: "Study Buddy",
-      adress: "https://github.com/fedu00/HR-study-buddy",
-      image: studyBuddy,
-      text: [
-        "Aplikacja do zarządzania studentami, tworzona w ramach kursu Reacta prowadzonego przez Adama Romańskiego tworzącego kanał programistyczny na youtube Hello Roman. ",
-      ],
-    },
-  ]
+  const API_TOKEN = "1ab3a70712337882e49d01c85666d9"
+
+  useEffect(() => {
+    axios
+      .post(
+        "https://graphql.datocms.com/",
+        {
+          query: `
+      {
+        allProjects {
+          id
+          title
+          githublink
+          content
+          image {
+            url
+          }
+        }
+      }
+      `,
+        },
+        {
+          headers: {
+            authorization: `Bearer ${API_TOKEN}`,
+          },
+        }
+      )
+      .then(({ data: { data } }) => setProjects(data.allProjects))
+      .catch((err) => console.log(err))
+  }, [])
+
+  console.log("projects", projects)
 
   return (
     <AnimateSection>
@@ -107,17 +102,12 @@ const Projects = () => {
         <Wrapper directioncolumn={true}>
           <h2>Projects</h2>
           <ProjectsWrapper>
-            {projectsData.map((element) => (
+            {projects.map(({ title, githublink, content, image }) => (
               <Project
-                image={element.image}
-                title={element.title}
+                image={image.url}
+                title={title}
                 handleOpenProjects={() =>
-                  handleOpenProjects(
-                    element.adress,
-                    element.image,
-                    element.title,
-                    element.text
-                  )
+                  handleOpenProjects(githublink, image.url, title, content)
                 }
               />
             ))}
