f134b25d7118a72daebc26e03788a6ac540979cf
Wojtek F <fedakwojciech@gmail.com>
2023-10-09 13:12:56 +0200

    add styles to home page, create social navigation bar and corect menu styles

---
+++ src/components/SocialNavBar/SocialNavBar.styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components"
+
+export const SocialNavBarContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  transform: translate(0, -50%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 50px 20px;
+  background-color: ${({ theme }) => theme.colors.myGreen};
+  border-radius: 0 30px 30px 0;
+  a {
+    margin: 15px 0;
+    transition: all 0.4s linear;
+    svg {
+      height: 30px;
+      fill: ${({ theme }) => theme.colors.backGroundColor};
+    }
+  }
+  a:hover {
+    scale: 1.4;
+  }
+  @media (max-width: 800px) {
+    display: none;
+  }
+`
--- src/components/mainTemplate/MainTemplate.js
+++ src/components/mainTemplate/MainTemplate.js
@@ -1,6 +1,6 @@
 import { MainTemplateWrapper } from "./MainTemplate.styles"
 import Navbar from "components/navbar/Navbar"
-import SocialMediaBar from "components/socialMediaBar/SocialMediaBar"
+// import SocialMediaBar from "components/socialMediaBar/SocialMediaBar"
 
 const MainTemplate = ({ children }) => {
   return (
